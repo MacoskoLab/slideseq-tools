a62f7f1e5c74a2070155a9befe23e646341ae527
James Webber <jwebber@broadinstitute.org>
2021-04-27 18:23:21 -0400

    double dangit!

--- src/slideseq/pipeline/alignment.py
+++ src/slideseq/pipeline/alignment.py
@@ -241,7 +241,7 @@ def main(
         "--readFilesCommand",
         "zcat",
         "--outFileNamePrefix",
-        f"{aligned_bam}",
+        f"{aligned_bam_prefix.with_suffix('.star.')}",
         "--outStd",
         "Log",
         "--outSAMtype",
