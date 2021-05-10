c1e0e91d9f6ac8ae89bd9365b8148239028961f1
James Webber <jwebber@broadinstitute.org>
2021-05-09 23:31:50 -0400

    so _many_ tiny bugs

--- src/slideseq/plot/plot_library_metrics.py
+++ src/slideseq/plot/plot_library_metrics.py
@@ -284,7 +284,7 @@ def make_library_plots(
     poly_a_summaries = [
         library_dir
         / f"L{lane:03d}"
-        / f"{manifest.flowcell}.L{lane:03d}.{row.library}.{row.barcode}.polyA_filtering.summary.txt"
+        / f"{manifest.flowcell}.L{lane:03d}.{row.library}.{row.sample_barcode}.polyA_filtering.summary.txt"
         for lane in lanes
     ]
 
