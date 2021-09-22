b19cbe74628d204ad87f0838f651d65e8a430909
Ali Qutab <aqutab@wm334-97b.broadinstitute.org>
2021-09-22 14:23:23 -0400

    ali_qutab edit 9

--- src/slideseq/plot/plot_downsampling.py
+++ src/slideseq/plot/plot_downsampling.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 
-# edit8 Ali Qutab
+# edit9 Ali Qutab
 # This script is to generate PDF for downsampling
-# same as edit 7 but took out [:10000]
+# took out [:10000] and changed ax.plot() to ax.scatter() - took out markersize and added in color
 
 import logging
 from pathlib import Path
@@ -19,12 +19,12 @@ log = logging.getLogger(__name__)
 def plot_downsampling(downsampling_output: list[tuple[float, Path]], figure_path: Path):
    xy = []
 
-   for ratio, downsample_summary in downsampling_output:
+   for r, downsample_summary in downsampling_output:
        _, umis_per_bc, _ = read_dge_summary(downsample_summary)
        # take the first 10000 barcodes as representative of real cells
-       v = np.mean(umis_per_bc)
+       data = np.mean(umis_per_bc)
 
-       xy.append((ratio, v))
+       xy.append((r, data))
 
    xy.sort()
    x, y = zip(*xy)
@@ -32,7 +32,7 @@ def plot_downsampling(downsampling_output: list[tuple[float, Path]], figure_path
    fig = matplotlib.figure.Figure(figsize=(8, 8))
    ax = fig.add_axes([0.1, 0.1, 0.8, 0.8])
 
-   ax.plot(x, y, marker="o", markersize=10, alpha=0.8)
+   ax.scatter(x, y, marker="o", alpha=0.8, color='r')
    ax.set_xlabel("Subsampling Ratio")
    ax.set_ylabel("Transcripts per barcode")
    ax.set_title("Average transcripts for all barcodes")
@@ -44,14 +44,14 @@ def plot_downsampling(downsampling_output: list[tuple[float, Path]], figure_path
 if __name__ == "__main__":
    # call the function here with input
    plot_downsampling(downsampling_output = [
-   (0.1, Path("/Users/aqutab/slideseq-tools/aqutab/aq_downsampling/aqutab_files/Puck_210203_04_0.1.digital_expression_summary.txt")),
-   (0.2, Path("/Users/aqutab/slideseq-tools/aqutab/aq_downsampling/aqutab_files/Puck_210203_04_0.2.digital_expression_summary.txt")),
-   (0.3, Path("/Users/aqutab/slideseq-tools/aqutab/aq_downsampling/aqutab_files/Puck_210203_04_0.3.digital_expression_summary.txt")),
-   (0.4, Path("/Users/aqutab/slideseq-tools/aqutab/aq_downsampling/aqutab_files/Puck_210203_04_0.4.digital_expression_summary.txt")),
-   (0.5, Path("/Users/aqutab/slideseq-tools/aqutab/aq_downsampling/aqutab_files/Puck_210203_04_0.5.digital_expression_summary.txt")),
-   (0.6, Path("/Users/aqutab/slideseq-tools/aqutab/aq_downsampling/aqutab_files/Puck_210203_04_0.6.digital_expression_summary.txt")),
-   (0.7, Path("/Users/aqutab/slideseq-tools/aqutab/aq_downsampling/aqutab_files/Puck_210203_04_0.7.digital_expression_summary.txt")),
-   (0.8, Path("/Users/aqutab/slideseq-tools/aqutab/aq_downsampling/aqutab_files/Puck_210203_04_0.8.digital_expression_summary.txt")),
-   (0.9, Path("/Users/aqutab/slideseq-tools/aqutab/aq_downsampling/aqutab_files/Puck_210203_04_0.9.digital_expression_summary.txt")),
-   (1.0, Path("/Users/aqutab/slideseq-tools/aqutab/aq_downsampling/aqutab_files/Puck_210203_04.matched.digital_expression_summary.txt"))
-   ], figure_path = Path("aq_edit8_plot_downsampling.png"))
+   (0.1, Path("/Users/aqutab/aqutab/aq_downsampling/aqutab_files/Puck_210203_04_0.1.digital_expression_summary.txt")),
+   (0.2, Path("/Users/aqutab/aqutab/aq_downsampling/aqutab_files/Puck_210203_04_0.2.digital_expression_summary.txt")),
+   (0.3, Path("/Users/aqutab/aqutab/aq_downsampling/aqutab_files/Puck_210203_04_0.3.digital_expression_summary.txt")),
+   (0.4, Path("/Users/aqutab/aqutab/aq_downsampling/aqutab_files/Puck_210203_04_0.4.digital_expression_summary.txt")),
+   (0.5, Path("/Users/aqutab/aqutab/aq_downsampling/aqutab_files/Puck_210203_04_0.5.digital_expression_summary.txt")),
+   (0.6, Path("/Users/aqutab/aqutab/aq_downsampling/aqutab_files/Puck_210203_04_0.6.digital_expression_summary.txt")),
+   (0.7, Path("/Users/aqutab/aqutab/aq_downsampling/aqutab_files/Puck_210203_04_0.7.digital_expression_summary.txt")),
+   (0.8, Path("/Users/aqutab/aqutab/aq_downsampling/aqutab_files/Puck_210203_04_0.8.digital_expression_summary.txt")),
+   (0.9, Path("/Users/aqutab/aqutab/aq_downsampling/aqutab_files/Puck_210203_04_0.9.digital_expression_summary.txt")),
+   (1.0, Path("/Users/aqutab/aqutab/aq_downsampling/aqutab_files/Puck_210203_04.matched.digital_expression_summary.txt"))
+   ], figure_path = Path("aq_edit9_plot_downsampling.png"))
