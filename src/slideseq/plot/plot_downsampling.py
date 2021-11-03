09f1ece9ac5c15da88151b89952dcb639340e6d8
Ali Qutab <aqutab@wm334-97b.broadinstitute.org>
2021-11-03 18:07:57 -0400

    2x and 10x depth in textbox, need to align textbox to top right

--- src/slideseq/plot/plot_downsampling.py
+++ src/slideseq/plot/plot_downsampling.py
@@ -144,9 +144,9 @@ def plot_downsampling(
     r_10 = (model(10.0, params)/model(1.0, params))
 
     # text box for a summary of the return for 2x and 10x depth for the 100% model
-    ax.text('2x depth for 100% model = 1.3287231945062343', transform=ax.transAxes, fontsize=9,
+    textstr = '\n'.join((('2x depth for 100% model = 1.3287231945062343'), ('10x depth for 100% model = 1.5199682596154036')))
+    ax.text(0.05, 0.05, textstr, transform=ax.transAxes, fontsize=9,
             verticalalignment='top', horizontalalignment='left', bbox=dict(boxstyle='round', facecolor='wheat', alpha=0.5))
-            # '10x depth for 100% model = 1.5199682596154036',
 
 
     FigureCanvasAgg(fig).print_figure(figure_path)
