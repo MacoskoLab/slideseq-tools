32be8a64d4f3691a3cc9ac86f4c2cc9dc0b8bb6e
James Webber <jwebber@broadinstitute.org>
2021-11-19 16:29:27 -0500

    replaced percentile calculation for efficiency

--- src/slideseq/plot/plot_downsampling.py
+++ src/slideseq/plot/plot_downsampling.py
@@ -18,64 +18,49 @@ log = logging.getLogger(__name__)
 def plot_downsampling(
     downsampling_output: list[tuple[float, Path]], matched_path: Path, figure_path: Path
 ):
-    xy = []
+    # percentiles of top barcodes to compute
+    percentiles = np.array([80, 60, 40, 20])
 
-    # right now barcodes is a list
     bc_list, full_umis_per_bc, _ = read_dge_summary(matched_path)
-    # this is a set comprehension, so we can remove the -1 from the matched barcodes
-    # bc.split("-") will split it into two parts, and we take the first one
+    # set comprehension to can remove the -1 from the matched barcodes
     bc_set = {bc.split("-")[0] for bc in bc_list}
-    data_100 = np.mean(full_umis_per_bc)
-    data_80 = np.mean(
-        full_umis_per_bc[: (int(0.8 * (len(full_umis_per_bc))))]
-    )  # for example, top 80% is from top item to item at 0.8 * number of items in list filtered_umis_per_bc
-    data_60 = np.mean(full_umis_per_bc[: (int(0.6 * (len(full_umis_per_bc))))])
-    data_40 = np.mean(full_umis_per_bc[: (int(0.4 * (len(full_umis_per_bc))))])
-    data_20 = np.mean(full_umis_per_bc[: (int(0.2 * (len(full_umis_per_bc))))])
-    xy.append((1.0, data_100, data_80, data_60, data_40, data_20))
 
-    for r, downsample_summary in downsampling_output:
-        if r == 1.0:
-            continue
-        else:
-            # read the barcodes and counts from this downsampled file
-            barcodes, umis_per_bc, _ = read_dge_summary(downsample_summary)
-            # will append barcodes that match barcodes in matched expression summary file
-            filtered_barcodes = []
-            # will append UMIs that match barcodes in matched expression summary file
-            filtered_umis_per_bc = []
-            # we zip together those lists so that we have each value as a pair
-            for bc, umis in zip(barcodes, umis_per_bc):
-                # checking for this is fast because it's a set
-                if bc in bc_set:
-                    filtered_barcodes.append(bc)
-                    filtered_umis_per_bc.append(umis)
-                    # take all barcodes as representative of real cells
-            data_100 = np.mean(filtered_umis_per_bc)
-            data_80 = np.mean(
-                filtered_umis_per_bc[: (int(0.8 * (len(filtered_umis_per_bc))))]
-            )  # for example, top 80% is from top item to item at 0.8 * number of items in list filtered_umis_per_bc
-            data_60 = np.mean(
-                filtered_umis_per_bc[: (int(0.6 * (len(filtered_umis_per_bc))))]
-            )
-            data_40 = np.mean(
-                filtered_umis_per_bc[: (int(0.4 * (len(filtered_umis_per_bc))))]
-            )
-            data_20 = np.mean(
-                filtered_umis_per_bc[: (int(0.2 * (len(filtered_umis_per_bc))))]
-            )
+    total_umis = sum(full_umis_per_bc)
+    n_bcs = len(full_umis_per_bc)
 
-        xy.append((r, data_100, data_80, data_60, data_40, data_20))
+    quintiles = total_umis - np.percentile(
+        np.cumsum(full_umis_per_bc[::-1]), percentiles[::-1]
+    )
+    quintiles /= n_bcs * percentiles / 100
+
+    xy = [(1.0, total_umis / n_bcs, *quintiles)]
+
+    for ratio, downsample_file in downsampling_output:
+        # read the barcodes and counts from this downsampled file
+        barcodes, umis_per_bc, _ = read_dge_summary(downsample_file)
+
+        # filter to barcodes from the matched expression summary file
+        r_umis_per_bc = [
+            umis for bc, umis in zip(barcodes, umis_per_bc) if bc in bc_set
+        ]
+
+        total_umis = sum(r_umis_per_bc)
+        n_bcs = len(r_umis_per_bc)
+
+        # calculate total UMIs for top 80, 40, 60, 20% of umis
+        quintiles = total_umis - np.percentile(
+            np.cumsum(r_umis_per_bc[::-1]), percentiles[::-1]
+        )
+        quintiles /= n_bcs * percentiles / 100
+
+        xy.append((ratio, total_umis / n_bcs, *quintiles))
 
     xy.sort()
-    x, y_100, y_80, y_60, y_40, y_20 = zip(*xy)
+    x, *ys = zip(*xy)
+
     # convert to arrays
     x = np.array(x)
-    y_100 = np.array(y_100)
-    y_80 = np.array(y_80)
-    y_60 = np.array(y_60)
-    y_40 = np.array(y_40)
-    y_20 = np.array(y_20)
+    ys = list(map(np.array, ys))
 
     def model(r, params):
         # y = alpha * exp(-r) + beta
@@ -85,28 +70,17 @@ def plot_downsampling(
 
         return params[0] * np.exp(-r) + params[1]
 
-    # model function takes 0.1, 0.2 ... 1.0 for r and outputs the mean counts values which are y,
-    # based on some parameters alpha and beta, which we need to find to predict the result for any value of r.
-
-    # The least_squares function is to optimize the model function, we want to minimize the error,
-    # which is the difference between the model prediction and the data.
-
     def model_least_squares(params, *, r, data):
         # computes model and compares it to the data
         return model(r, params) - data
 
-    x_values = np.linspace(
-        0.1, 3.0, 30
-    )  # this function creates a linear space of points: 30 points from 0.1 to 3.0 (0.1, 0.2, ... 2.9, 3.0)
+    x_values = np.linspace(0.1, 3.0, 30)
+    parameters = dict()
 
     fig = matplotlib.figure.Figure(figsize=(8, 8))
     ax = fig.add_axes([0.1, 0.1, 0.8, 0.8])
 
-    for y, label, color in zip(
-        (y_100, y_80, y_60, y_40, y_20),
-        ("100", "80", "60", "40", "20"),
-        ("r", "b", "g", "k", "p"),
-    ):
+    for y, label in zip(ys, (100, *percentiles)):
         output = scipy.optimize.least_squares(
             model_least_squares,
             [-10.0, 10.0],  # initial values for params
@@ -118,9 +92,8 @@ def plot_downsampling(
             method="dogbox",  # I found this method to work well for this problem
         )
 
-        params = output.x
-
-        pred_y = model(x_values, params)
+        parameters[label] = output.x
+        pred_y = model(x_values, parameters[label])
 
         ax.scatter(x, y, marker="o", alpha=0.8)
         ax.plot(x_values, pred_y, label=f"top {label}%", alpha=0.8)
@@ -131,18 +104,19 @@ def plot_downsampling(
 
     # r went up to 1.0 for actual data, but x_values for model go up to 3.0
     ax.set_xlim(0.0, 3.1)
-
     ax.legend()
 
-    # calculate 2x and 10x depth for the 100% model with the ratios model(2) / model(1) and model(10) / model(1)
-    r_2 = model(2.0, params) / model(1.0, params)
-    r_10 = model(10.0, params) / model(1.0, params)
+    # calculate 2x and 10x depth for the 100% model with the
+    # ratios model(2) / model(1) and model(10) / model(1)
+    r_2 = model(2.0, parameters[20]) / model(1.0, parameters[20])
+    r_10 = model(10.0, parameters[20]) / model(1.0, parameters[20])
 
     # text box, bottom right, for a summary of the return for 2x and 10x depth for the 100% model
-    textstr = f"top 20%, 2x depth:  {r_2:.1%}\ntop 20%, 10x depth: {r_10:.1%}"
+    textstr = f"top 20%, 2x depth: {r_2:8.1%}\ntop 20%, 10x depth: {r_10:6.1%}"
     # use AnchoredText to position text box to bottom right
     at = AnchoredText(textstr, loc="lower right", prop=dict(size=8), frameon=True)
     at.patch.set_boxstyle("round,pad=0.,rounding_size=0.2")
+    at.patch.set_edgecolor((0.8, 0.8, 0.8))
     ax.add_artist(at)
 
     FigureCanvasAgg(fig).print_figure(figure_path)
@@ -155,9 +129,9 @@ if __name__ == "__main__":
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
     parser.add_argument(
-        "ds_path", nargs="+", type=Path, help="Path of a downsample_summary text file"
+        "ds_path", nargs="+", type=Path, help="Downsampled summary files"
     )
-    parser.add_argument("--m_path", type=Path, help="Path of match text file")
+    parser.add_argument("--m_path", type=Path, help="Matched summary file")
     parser.add_argument("--output", help="output filename", required=True)
     args = parser.parse_args()
 
@@ -167,9 +141,9 @@ if __name__ == "__main__":
     # for 0.1, 0.2,... files
     for downsample_summary in args.ds_path:
         # extract ratio from file name
-        r = float(downsample_summary.name.split("_")[3][:3])
+        downsample_ratio = float(downsample_summary.name.split("_")[3][:3])
 
-        downsampling_list.append((r, downsample_summary))
+        downsampling_list.append((downsample_ratio, downsample_summary))
 
     plot_downsampling(
         downsampling_list, matched_path=args.m_path, figure_path=args.output
